a761e6c9258004cf2c58cb762843e12c1c87ce7f
puer <vova@mytnik.net>
2019-11-13 14:11:28 +0300

    route and redux provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Row, Col } from 'antd';
 import Login from './Login';
+import Register from './Register';
+import User from './User';
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Row type="flex" justify="start">
+      <Col span={8}>
+        <Router>
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <User />
+            </Route>
+          </Switch>
+        </Router>
+      </Col>
+    </Row>
   );
 }
 
